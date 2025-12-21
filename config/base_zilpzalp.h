f65082f7843589ad5f5e0aa95ed93236bfa47b4f
Mateja Opacic <mateja.opacic@gmail.com>
2025-12-21 14:07:33 +0100

    Fixing SYM layer 4

--- config/base_zilpzalp.h
+++ config/base_zilpzalp.h
@@ -127,6 +127,16 @@
     };
   };
 };
+macro_dquote: macro_dquote {
+  label = "Double Quote";
+  compatible = "zmk,behavior-macro";
+  #binding-cells = <0>;
+  bindings = <
+    &macro_press   &kp LALT
+    &macro_tap     &kp W
+    &macro_release &kp LALT
+  >;
+};
 
 
 #define COMBO(NAME, BINDINGS, KEYPOS) \
